4eb0494702b74e5dae8224d94fe2114683fa564a
An Vad <anil.vadlapatla@gmail.com>
2016-08-02 05:50:44 -0700

    bwt done. Good job! (Max time used: 0.02/0.50, max memory used: 9007104/536870912.)

--- strings_w2/bwt/bwt.py
+++ strings_w2/bwt/bwt.py
@@ -1,9 +1,28 @@
 # python3
+
+# Good job! (Max time used: 0.02/0.50, max memory used: 9007104/536870912.)
+
+
 import sys
 
-def BWT(text):
-    return ""
+
+def bwt(text):
+    """Constructs Burrows–Wheeler transform."""
+    bwm = [] # holds the Burrows-Wheeler matrix of given text
+    for i in range(len(text)):
+        bwm.append(text[i:] + text[0:i])
+    bwm_sorted = sorted(bwm)
+    #print(bwm_sorted)
+    bwt_list = []
+    for i in range(len(text)):
+        bwt_list.append(bwm_sorted[i][-1])
+    return "".join(bwt_list)
+
+
+def main():
+    text = sys.stdin.readline().strip()
+    print(bwt(text))
+
 
 if __name__ == '__main__':
-    text = sys.stdin.readline().strip()
-    print(BWT(text))
+    main()
